46388974fa71a19902d4e6d99a378823e3f12e7a
xyyx <xyyx@mail.ru>
2017-09-09 10:12:48 +0300

    base: Lockscreen Charging info  (3/3)
         based on: [2/2] frameworks: show charging current & voltage on lockscreen by
         yank555-lu [2/2]Base: show more battery info on lockscreen when charging by
         yank555-lu LockscreenCharging: Use double for mChargingVoltage and show as
         4.XV by xyyx Lockscreen charging: Formatting improvements by nathanchance
    
        AICPfy:
        - default set to false
        - Current formatting improvement for devices reporting directly in Amps
    
        Return-logic of computePowerIndication has changed in Q, so adapted
        it to the new logic.
    
        Ported to 11 & 12 by @eyosen: adapt to the move to SettingsLib
    
    Change-Id: Icc08f6f7b0fd31cb5ab31556c6a18ee9bc57bf22

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5653,6 +5653,12 @@ public final class Settings {
          */
         public static final String NETWORK_TRAFFIC_AUTOHIDE_THRESHOLD = "network_traffic_autohide_threshold";
 
+        /**
+         * Whether to show the battery info on the lockscreen while charging
+         * @hide
+         */
+        public static final String LOCKSCREEN_BATTERY_INFO = "lockscreen_battery_info";
+
         /***************************
          * AICP System Settings end
          ***************************/
@@ -5801,6 +5807,7 @@ public final class Settings {
             PRIVATE_SETTINGS.add(HIGH_TOUCH_SENSITIVITY_ENABLE);
             PRIVATE_SETTINGS.add(NETWORK_TRAFFIC_STATE);
             PRIVATE_SETTINGS.add(NETWORK_TRAFFIC_AUTOHIDE_THRESHOLD);
+            PRIVATE_SETTINGS.add(LOCKSCREEN_BATTERY_INFO);
         }
 
         /**
