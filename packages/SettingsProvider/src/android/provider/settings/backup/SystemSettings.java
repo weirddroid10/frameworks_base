a5bc7db12d701d793ec0f8df93d9574cb8b7bdfb
calebcabob <calphonic@gmail.com>
2021-01-06 15:50:57 +0300

    base: LockscreenCharging: Bring in °F/°C toggle [1/2]
    Extracted from
    https://review.aosip.dev/c/AOSIP/platform_frameworks_base/+/16594
    
    Default updated to celcius
    
    Change-Id: If618c5235d4864c8e68d6f266d1012f3ba64ba3b

--- packages/SettingsProvider/src/android/provider/settings/backup/SystemSettings.java
+++ packages/SettingsProvider/src/android/provider/settings/backup/SystemSettings.java
@@ -95,5 +95,6 @@ public class SystemSettings {
         Settings.System.NETWORK_TRAFFIC_STATE,
         Settings.System.NETWORK_TRAFFIC_AUTOHIDE_THRESHOLD,
         Settings.System.LOCKSCREEN_BATTERY_INFO,
+        Settings.System.LOCKSCREEN_BATTERY_INFO_TEMP_UNIT,
     };
 }
